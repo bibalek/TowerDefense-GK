89026d1913ff37e2845df66947b5dea80080227f
PWylezalek <piotwyl539@student.polsl.pl>
2017-05-28 20:08:49 +0200

    Added finding closest enemy by Turret

---
+++ TowerDefense/Assets/Scripts/Turret.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+
+    [SerializeField]
+    float range;
+    [SerializeField]
+    float searchCooldown;
+    [SerializeField]
+    GameObject partRotatingInYAxis;
+
+    private Collider[] enemies;
+    private Collider closestEnemy;
+    private float minimumDistance = Mathf.Infinity;
+    private float currentDistance;
+    private int enemyLayer;
+    private bool canSearch = true;
+
+    void Start()
+    {
+        enemyLayer = 1 << LayerMask.NameToLayer("Enemy");
+    }
+
+    void Update()
+    {
+        TryToSearchForEnemy();
+        TryToRotateTowardsEnemy();
+    }
+
+    private void TryToSearchForEnemy()
+    {
+        if (canSearch)
+        {
+            SearchForNewTarget();
+            StartCoroutine(Cooldown(searchCooldown));
+        }
+    }
+
+    private void TryToRotateTowardsEnemy()
+    {
+        if (closestEnemy != null && EnemyInRange())
+        {
+            RotateTowardEnemy();
+        }
+    }
+
+    private bool EnemyInRange()
+    {
+        if(Vector3.Distance(transform.position, closestEnemy.transform.position) > range)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    private void RotateTowardEnemy()
+    {
+        Quaternion lookRotation = Quaternion.LookRotation(closestEnemy.transform.position - transform.position);
+        partRotatingInYAxis.transform.rotation = Quaternion.Euler(0, lookRotation.eulerAngles.y, 0);
+    }
+
+    private IEnumerator Cooldown(float time)
+    {
+        canSearch = false;
+        yield return new WaitForSeconds(time);
+        canSearch = true;
+    }
+
+    private void SearchForNewTarget()
+    {
+        closestEnemy = null;
+        minimumDistance = Mathf.Infinity;
+        enemies = Physics.OverlapSphere(transform.position, range, enemyLayer);
+        foreach (Collider col in enemies)
+        {
+            if (col.transform.parent.CompareTag("Enemy"))
+            {
+                FindClosestEnemy(col);
+            }
+        }
+    }
+
+    private void FindClosestEnemy(Collider collider)
+    {
+        currentDistance = Vector3.Distance(transform.position, collider.transform.position);
+        if (currentDistance < minimumDistance)
+        {
+            minimumDistance = currentDistance;
+            closestEnemy = collider;
+        }
+    }
+}
