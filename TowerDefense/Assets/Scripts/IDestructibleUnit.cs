e94f2fc5b91bd4481f4d741860d396f5816a10ae
PWylezalek <piotwyl539@student.polsl.pl>
2017-06-06 01:35:22 +0200

    Added IDestructibleUnit.cs, Enemy.cs, changed gameEventController

---
+++ TowerDefense/Assets/Scripts/IDestructibleUnit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+interface IDestructibleUnit
+{
+    void DealDamage(int damageToDeal);
+
+    void DestroyUnit();
+
+}
+
